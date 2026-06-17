4e7e3b1fafe4bbec9895819dcd3ea8efa18ceaf3
Matheus Leander Vieira <leander7.pmf@gmail.com>
2026-06-17 19:51:45 -0300

    feat: classe Filme

---
+++ Filme.cs
@@ -0,0 +1,16 @@
+﻿public class Filme
+{
+    public string titulo;
+    public string genero;
+    public int anoLancamento;
+    public int classificacaoIndicativa;
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine("\n=== Filme Cadastrado ===");
+        Console.WriteLine("Título: " + titulo);
+        Console.WriteLine("Gênero: " + genero);
+        Console.WriteLine("Ano de lançamento: " + anoLancamento);
+        Console.WriteLine("Classificação: " + classificacaoIndicativa);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Filme filme = new Filme();
+filme.titulo = "Avatar";
+filme.genero = "Aventura";
+filme.anoLancamento = 2009;
+filme.classificacaoIndicativa = 12;
+
+filme.ExibirInformacoes();
