b379ae7b249fb79e7a0895ee94f3c2798fc8d2a4
Matheus Leander Vieira <leander7.pmf@gmail.com>
2026-06-17 21:31:47 -0300

    feat: implementa entarda de dados do usuário

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 ﻿Filme filme = new Filme();
-filme.titulo = "Avatar";
-filme.genero = "Aventura";
-filme.anoLancamento = 2009;
-filme.classificacaoIndicativa = 12;
+
+Console.Write("Título: ");
+filme.titulo = Console.ReadLine();
+
+Console.Write("Gênero: ");
+filme.genero = Console.ReadLine();
+
+Console.Write("Ano de Lançamento: ");
+filme.anoLancamento = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Classificação Indicativa: ");
+filme.classificacaoIndicativa = Convert.ToInt32(Console.ReadLine());
 
 filme.ExibirInformacoes();
