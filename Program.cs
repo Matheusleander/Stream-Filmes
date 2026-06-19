45e418e85284d447def8c45b40536efe39b91431
Matheus Leander Vieira <leander7.pmf@gmail.com>
2026-06-18 21:22:38 -0300

    feat: implementa menu principal

--- Program.cs
+++ Program.cs
@@ -1,16 +1,54 @@
 ﻿Filme filme = new Filme();
 
-Console.Write("Título: ");
-filme.titulo = Console.ReadLine();
+int opcao;
 
-Console.Write("Gênero: ");
-filme.genero = Console.ReadLine();
+do
+{
+    Console.WriteLine("\n=====CATÁLOGO DE FILMES=====");
+    Console.WriteLine("1 - Cadastrar Filme");
+    Console.WriteLine("2 - Exibir Filme");
+    Console.WriteLine("0 - Sair");
+    Console.WriteLine("Escolha uma opção: ");
+    opcao = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Ano de Lançamento: ");
-filme.anoLancamento = Convert.ToInt32(Console.ReadLine());
+    switch (opcao)
+    {
+        case 1:
+            Console.Write("Título: ");
+            filme.titulo = Console.ReadLine();
+
+            Console.Write("Gênero: ");
+            filme.genero = Console.ReadLine();
+
+            Console.Write("Ano de Lançamento: ");
+            filme.anoLancamento = Convert.ToInt32(Console.ReadLine());
 
 
-Console.Write("Classificação Indicativa: ");
-filme.classificacaoIndicativa = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Classificação Indicativa: ");
+            filme.classificacaoIndicativa = Convert.ToInt32(Console.ReadLine());
 
-filme.ExibirInformacoes();
+            Console.WriteLine("\nFilme cadastrado com sucesso!");
+            break;
+        case 2:
+            if (string.IsNullOrEmpty(filme.titulo))
+            {
+                Console.WriteLine("\nNenhum filme cadastrado ainda :(");
+            }
+            else
+            {
+                filme.ExibirInformacoes();
+            }
+            break;
+        case 0:
+            Console.WriteLine("\nAtenção!!! Programa encerrado.");
+            break;
+        default:
+            Console.WriteLine("\nOpção inválida");
+            break;
+    }
+} while (opcao != 0);
+
+
+
+
+
