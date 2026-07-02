34d4758b91cd62d6cabc4c00375556006b7a107b
Matheus Leander Vieira <leander7.pmf@gmail.com>
2026-07-01 21:16:19 -0300

    feat: altera armazenamento para lista de filmes

--- Program.cs
+++ Program.cs
@@ -1,54 +1,113 @@
-﻿Filme filme = new Filme();
+﻿using System;
+using System.Collections.Generic;
 
-int opcao;
-
-do
+namespace StreamFilmes
 {
-    Console.WriteLine("\n=====CATÁLOGO DE FILMES=====");
-    Console.WriteLine("1 - Cadastrar Filme");
-    Console.WriteLine("2 - Exibir Filme");
-    Console.WriteLine("0 - Sair");
-    Console.WriteLine("Escolha uma opção: ");
-    opcao = Convert.ToInt32(Console.ReadLine());
-
-    switch (opcao)
+    class Program
     {
-        case 1:
-            Console.Write("Título: ");
-            filme.titulo = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            List<Filme> listaFilmes = new List<Filme>();
+            int opcao;
 
-            Console.Write("Gênero: ");
-            filme.genero = Console.ReadLine();
-
-            Console.Write("Ano de Lançamento: ");
-            filme.anoLancamento = Convert.ToInt32(Console.ReadLine());
-
-
-            Console.Write("Classificação Indicativa: ");
-            filme.classificacaoIndicativa = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("\nFilme cadastrado com sucesso!");
-            break;
-        case 2:
-            if (string.IsNullOrEmpty(filme.titulo))
+            do
             {
-                Console.WriteLine("\nNenhum filme cadastrado ainda :(");
-            }
-            else
-            {
-                filme.ExibirInformacoes();
-            }
-            break;
-        case 0:
-            Console.WriteLine("\nAtenção!!! Programa encerrado.");
-            break;
-        default:
-            Console.WriteLine("\nOpção inválida");
-            break;
+                Console.WriteLine("\n===== CATÁLOGO DE FILMES =====");
+                Console.WriteLine("1 - Cadastrar Filme");
+                Console.WriteLine("2 - Exibir Todos os Filmes");
+                Console.WriteLine("3 - Buscar Filme por Título");
+                Console.WriteLine("0 - Sair");
+                Console.Write("Escolha uma opção: ");
+                opcao = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+
+                        Filme novoFilme = new Filme();
+
+                        Console.Write("Título: ");
+                        novoFilme.titulo = Console.ReadLine();
+
+                        Console.Write("Gênero: ");
+                        novoFilme.genero = Console.ReadLine();
+
+                        // Validação do ano
+                        int ano;
+                        do
+                        {
+                            Console.Write("Ano de lançamento (1888 até 2026): ");
+                            ano = Convert.ToInt32(Console.ReadLine());
+
+                            if (ano < 1888 || ano > 2026)
+                            {
+                                Console.WriteLine("Ano inválido! Digite um ano real entre 1888 e 2026.");
+                            }
+
+                        } while (ano < 1888 || ano > 2026);
+
+                        novoFilme.anoLancamento = ano;
+
+                        Console.Write("Classificação indicativa: ");
+                        novoFilme.classificacaoIndicativa = Convert.ToInt32(Console.ReadLine());
+
+                        listaFilmes.Add(novoFilme);
+
+                        Console.WriteLine("\nFilme cadastrado com sucesso!");
+                        break;
+
+                    case 2:
+
+                        if (listaFilmes.Count == 0)
+                        {
+                            Console.WriteLine("\nNenhum filme cadastrado ainda.");
+                        }
+                        else
+                        {
+                            foreach (var filme in listaFilmes)
+                            {
+                                filme.ExibirInformacoes();
+                            }
+                        }
+
+                        break;
+
+                    case 3:
+
+                        Console.Write("Digite o título para buscar: ");
+                        string termoBusca = Console.ReadLine();
+
+                        bool encontrado = false;
+
+                        foreach (var filme in listaFilmes)
+                        {
+                            if (filme.titulo.ToLower().Contains(termoBusca.ToLower()))
+                            {
+                                filme.ExibirInformacoes();
+                                encontrado = true;
+                            }
+                        }
+
+                        if (!encontrado)
+                        {
+                            Console.WriteLine("\nNenhum filme encontrado com esse termo.");
+                        }
+                        Console.WriteLine("\nPressione qualquer tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+
+                    case 0:
+
+                        Console.WriteLine("\nPrograma encerrado.");
+                        break;
+
+                    default:
+
+                        Console.WriteLine("\nOpção inválida.");
+                        break;
+                }
+
+            } while (opcao != 0);
+        }
     }
-} while (opcao != 0);
-
-
-
-
-
+}
